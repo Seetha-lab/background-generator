37e32c6d8e4fda8037c20ac16f550c976fd6fa11
Seetha-lab <63551940+Seetha-lab@users.noreply.github.com>
2020-05-17 08:09:47 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var test = require('lodash');
-//console.log(test);
-
-var array=[1,2,3,4,5,6,7,8];
-console.log("without",test.without(array,8));
-console.log("concat",test.concat(array,4,"a",[1],[1,2,3,[4,5,6]]));
-
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var h3=document.querySelector("h3");
